a694c7c041b1a58c5d3acf450422fc2968680145
multiplex55 <6619098+multiplex55@users.noreply.github.com>
2025-07-07 10:37:49 -0400

    Make command help window scrollable

--- src/help_window.rs
+++ src/help_window.rs
@@ -14,11 +14,24 @@ impl HelpWindow {
         let mut open = self.open;
         egui::Window::new("Command Help")
             .open(&mut open)
+            .resizable(true)
+            .default_size((400.0, 300.0))
             .show(ctx, |ui| {
-                ui.label("Available commands:");
-                for (_, desc, _) in app.plugins.plugin_infos() {
-                    ui.label(desc);
-                }
+                ui.vertical_centered(|ui| ui.heading("Available commands"));
+                ui.separator();
+                let mut infos = app.plugins.plugin_infos();
+                infos.sort_by(|a, b| a.0.cmp(&b.0));
+                let area_height = ui.available_height();
+                egui::ScrollArea::vertical()
+                    .max_height(area_height)
+                    .show(ui, |ui| {
+                        for (name, desc, _) in infos {
+                            ui.horizontal(|ui| {
+                                ui.monospace(format!("{name:>8}"));
+                                ui.label(desc);
+                            });
+                        }
+                    });
             });
         self.open = open;
     }
