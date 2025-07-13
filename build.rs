d8c143f3dffb279759fe7e41668ef0c8d8e8746a
multiplex55 <6619098+multiplex55@users.noreply.github.com>
2025-07-13 18:50:54 -0400

    Use embed_resource NONE constant

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 #[cfg(target_os = "windows")]
 fn main() {
     // no custom macros are passed to the resource compiler
-    embed_resource::compile("Resources/windows.rc", &[]);
+    embed_resource::compile("Resources/windows.rc", embed_resource::NONE);
 }
 
 #[cfg(not(target_os = "windows"))]
