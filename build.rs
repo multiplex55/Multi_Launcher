1288d6f7df6a84f2e1594aa37c7a2a037690157b
multiplex55 <6619098+multiplex55@users.noreply.github.com>
2025-07-13 17:44:40 -0400

    Fix icon generation by resizing image

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 #[cfg(target_os = "windows")]
 fn main() {
+    use image::imageops::FilterType;
     use std::env;
     use std::fs::File;
     use std::path::Path;
@@ -9,7 +10,12 @@ fn main() {
 
     let bytes = include_bytes!("Resources/Green_MultiLauncher.png");
     let img = image::load_from_memory(bytes).expect("load icon");
-    img.write_to(&mut File::create(&ico_path).expect("create ico"), image::ImageOutputFormat::Ico).expect("write ico");
+    let icon = img.resize(256, 256, FilterType::Lanczos3);
+    icon.write_to(
+        &mut File::create(&ico_path).expect("create ico"),
+        image::ImageOutputFormat::Ico,
+    )
+    .expect("write ico");
 
     let mut res = winres::WindowsResource::new();
     res.set_icon(ico_path.to_str().unwrap());
