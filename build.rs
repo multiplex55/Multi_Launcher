fa4b0d3d6ed903c2ec60f943e2dd845487f2770a
multiplex55 <6619098+multiplex55@users.noreply.github.com>
2025-07-13 17:52:01 -0400

    Generate multi-size Windows icon

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 #[cfg(target_os = "windows")]
 fn main() {
+    use image::codecs::ico::{IcoEncoder, IcoFrame};
     use image::imageops::FilterType;
+    use image::ColorType;
     use std::env;
     use std::fs::File;
     use std::path::Path;
@@ -10,12 +12,20 @@ fn main() {
 
     let bytes = include_bytes!("Resources/Green_MultiLauncher.png");
     let img = image::load_from_memory(bytes).expect("load icon");
-    let icon = img.resize(256, 256, FilterType::Lanczos3);
-    icon.write_to(
-        &mut File::create(&ico_path).expect("create ico"),
-        image::ImageOutputFormat::Ico,
-    )
-    .expect("write ico");
+
+    let mut frames = Vec::new();
+    for &size in &[16u32, 32, 48, 256] {
+        let resized = img.resize_exact(size, size, FilterType::Lanczos3).to_rgba8();
+        frames.push(
+            IcoFrame::as_png(resized.as_raw(), size, size, ColorType::Rgba8)
+                .expect("create ico frame"),
+        );
+    }
+
+    let mut file = File::create(&ico_path).expect("create ico");
+    IcoEncoder::new(&mut file)
+        .encode_images(&frames)
+        .expect("write ico");
 
     let mut res = winres::WindowsResource::new();
     res.set_icon(ico_path.to_str().unwrap());
