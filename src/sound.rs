b5b2325bef87f71079b463989d7086d9df684555
multiplex55 <6619098+multiplex55@users.noreply.github.com>
2025-07-15 19:29:38 -0400

    Fix sound playback

--- src/sound.rs
+++ src/sound.rs
@@ -22,15 +22,42 @@ pub static SOUND_NAMES: &[&str] = &[
 static SOUNDS: Lazy<Vec<(&'static str, &'static [u8])>> = Lazy::new(|| {
     vec![
         ("Alarm.wav", include_bytes!("../Resources/sounds/Alarm.wav")),
-        ("Alarm02.wav", include_bytes!("../Resources/sounds/Alarm02.wav")),
-        ("Alarm03.wav", include_bytes!("../Resources/sounds/Alarm03.wav")),
-        ("Alarm04.wav", include_bytes!("../Resources/sounds/Alarm04.wav")),
-        ("Alarm05.wav", include_bytes!("../Resources/sounds/Alarm05.wav")),
-        ("Alarm06.wav", include_bytes!("../Resources/sounds/Alarm06.wav")),
-        ("Alarm07.wav", include_bytes!("../Resources/sounds/Alarm07.wav")),
-        ("Alarm08.wav", include_bytes!("../Resources/sounds/Alarm08.wav")),
-        ("Alarm09.wav", include_bytes!("../Resources/sounds/Alarm09.wav")),
-        ("AlarmNag.wav", include_bytes!("../Resources/sounds/AlarmNag.wav")),
+        (
+            "Alarm02.wav",
+            include_bytes!("../Resources/sounds/Alarm02.wav"),
+        ),
+        (
+            "Alarm03.wav",
+            include_bytes!("../Resources/sounds/Alarm03.wav"),
+        ),
+        (
+            "Alarm04.wav",
+            include_bytes!("../Resources/sounds/Alarm04.wav"),
+        ),
+        (
+            "Alarm05.wav",
+            include_bytes!("../Resources/sounds/Alarm05.wav"),
+        ),
+        (
+            "Alarm06.wav",
+            include_bytes!("../Resources/sounds/Alarm06.wav"),
+        ),
+        (
+            "Alarm07.wav",
+            include_bytes!("../Resources/sounds/Alarm07.wav"),
+        ),
+        (
+            "Alarm08.wav",
+            include_bytes!("../Resources/sounds/Alarm08.wav"),
+        ),
+        (
+            "Alarm09.wav",
+            include_bytes!("../Resources/sounds/Alarm09.wav"),
+        ),
+        (
+            "AlarmNag.wav",
+            include_bytes!("../Resources/sounds/AlarmNag.wav"),
+        ),
         (
             "ReminderDelete.wav",
             include_bytes!("../Resources/sounds/ReminderDelete.wav"),
@@ -43,7 +70,10 @@ static SOUNDS: Lazy<Vec<(&'static str, &'static [u8])>> = Lazy::new(|| {
             "ReminderStart.wav",
             include_bytes!("../Resources/sounds/ReminderStart.wav"),
         ),
-        ("StartUp.wav", include_bytes!("../Resources/sounds/StartUp.wav")),
+        (
+            "StartUp.wav",
+            include_bytes!("../Resources/sounds/StartUp.wav"),
+        ),
     ]
 });
 
@@ -51,17 +81,18 @@ pub fn play_sound(name: &str) {
     if name == "None" {
         return;
     }
-    let data = SOUNDS
-        .iter()
-        .find(|(n, _)| *n == name)
-        .map(|(_, d)| *d);
-    let Some(bytes) = data else { return; };
-    if let Ok((_stream, handle)) = rodio::OutputStream::try_default() {
-        if let Ok(source) = rodio::Decoder::new(Cursor::new(bytes)) {
-            if let Ok(sink) = rodio::Sink::try_new(&handle) {
-                sink.append(source);
-                sink.detach();
+    let data = SOUNDS.iter().find(|(n, _)| *n == name).map(|(_, d)| *d);
+    let Some(bytes) = data else {
+        return;
+    };
+    std::thread::spawn(move || {
+        if let Ok((_stream, handle)) = rodio::OutputStream::try_default() {
+            if let Ok(source) = rodio::Decoder::new(Cursor::new(bytes)) {
+                if let Ok(sink) = rodio::Sink::try_new(&handle) {
+                    sink.append(source);
+                    let _ = sink.sleep_until_end();
+                }
             }
         }
-    }
+    });
 }
