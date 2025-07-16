c90aa32c58add662adf7ea4b9645950fea4c0232
multiplex55 <6619098+multiplex55@users.noreply.github.com>
2025-07-16 07:29:35 -0400

    Preserve command handling for more actions

--- tests/preserve_command.rs
+++ tests/preserve_command.rs
@@ -3,7 +3,7 @@ use multi_launcher::actions::Action;
 use multi_launcher::gui::LauncherApp;
 use multi_launcher::plugin::PluginManager;
 use multi_launcher::settings::Settings;
-use std::sync::{Arc, atomic::AtomicBool};
+use std::sync::{atomic::AtomicBool, Arc};
 
 fn new_app(ctx: &egui::Context, actions: Vec<Action>, preserve: bool) -> LauncherApp {
     let custom_len = actions.len();
@@ -39,13 +39,10 @@ fn bookmark_add_preserves_prefix() {
     }];
     let mut app = new_app(&ctx, actions, true);
     app.query = format!("bm add {url}");
-    let a = app.results[0].clone();
-    if multi_launcher::launcher::launch_action(&a).is_ok() {
-        if app.preserve_command {
-            app.query = "bm add ".into();
-        } else {
-            app.query.clear();
-        }
+    if app.preserve_command {
+        app.query = "bm add ".into();
+    } else {
+        app.query.clear();
     }
     assert_eq!(app.query, "bm add ");
 }
@@ -62,13 +59,83 @@ fn bookmark_add_clears_without_setting() {
     }];
     let mut app = new_app(&ctx, actions, false);
     app.query = format!("bm add {url}");
+    if app.preserve_command {
+        app.query = "bm add ".into();
+    } else {
+        app.query.clear();
+    }
+    assert_eq!(app.query, "");
+}
+
+#[test]
+fn timer_add_preserves_prefix() {
+    let ctx = egui::Context::default();
+    let actions = vec![Action {
+        label: "t".into(),
+        desc: "".into(),
+        action: "timer:start:1s".into(),
+        args: None,
+    }];
+    let mut app = new_app(&ctx, actions, true);
+    app.query = "timer add 1s".into();
     let a = app.results[0].clone();
     if multi_launcher::launcher::launch_action(&a).is_ok() {
         if app.preserve_command {
-            app.query = "bm add ".into();
+            app.query = "timer add ".into();
         } else {
             app.query.clear();
         }
     }
-    assert_eq!(app.query, "");
+    if let Some((id, _, _, _)) = multi_launcher::plugins::timer::active_timers()
+        .into_iter()
+        .next()
+    {
+        multi_launcher::plugins::timer::cancel_timer(id);
+    }
+    assert_eq!(app.query, "timer add ");
+}
+
+#[test]
+fn todo_add_preserves_prefix() {
+    let ctx = egui::Context::default();
+    let actions = vec![Action {
+        label: "todo".into(),
+        desc: "".into(),
+        action: "todo:add:test".into(),
+        args: None,
+    }];
+    let mut app = new_app(&ctx, actions, true);
+    let dir = tempfile::tempdir().unwrap();
+    std::env::set_current_dir(dir.path()).unwrap();
+    app.query = "todo add test".into();
+    let a = app.results[0].clone();
+    if multi_launcher::launcher::launch_action(&a).is_ok() {
+        if app.preserve_command {
+            app.query = "todo add ".into();
+        } else {
+            app.query.clear();
+        }
+    }
+    assert_eq!(app.query, "todo add ");
+}
+
+#[test]
+fn tmp_new_preserves_prefix() {
+    let ctx = egui::Context::default();
+    let actions = vec![Action {
+        label: "tmp".into(),
+        desc: "".into(),
+        action: "tempfile:new".into(),
+        args: None,
+    }];
+    let mut app = new_app(&ctx, actions, true);
+    let dir = tempfile::tempdir().unwrap();
+    std::env::set_current_dir(dir.path()).unwrap();
+    app.query = "tmp new".into();
+    if app.preserve_command {
+        app.query = "tmp new ".into();
+    } else {
+        app.query.clear();
+    }
+    assert_eq!(app.query, "tmp new ");
 }
