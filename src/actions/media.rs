0b5e283ddece0da051c5291c5d55a23d8cdddbee
multiplex55 <6619098+multiplex55@users.noreply.github.com>
2025-08-24 15:38:14 -0400

    refactor: remove linux media player integration

--- src/actions/media.rs
+++ src/actions/media.rs
@@ -1,4 +1,3 @@
-#[cfg(target_os = "windows")]
 fn send_key(vk: windows::Win32::UI::Input::KeyboardAndMouse::VIRTUAL_KEY) {
     use windows::Win32::UI::Input::KeyboardAndMouse::{
         SendInput, INPUT, INPUT_0, INPUT_KEYBOARD, KEYBDINPUT, KEYBD_EVENT_FLAGS, KEYEVENTF_KEYUP,
@@ -22,79 +21,26 @@ fn send_key(vk: windows::Win32::UI::Input::KeyboardAndMouse::VIRTUAL_KEY) {
     }
 }
 
-#[cfg(target_os = "linux")]
-fn playerctl(cmd: &str) {
-    let _ = std::process::Command::new("playerctl").arg(cmd).spawn();
-}
-
-#[cfg(target_os = "windows")]
 pub fn play() -> anyhow::Result<()> {
     use windows::Win32::UI::Input::KeyboardAndMouse::VK_MEDIA_PLAY_PAUSE;
     send_key(VK_MEDIA_PLAY_PAUSE);
     Ok(())
 }
 
-#[cfg(target_os = "linux")]
-pub fn play() -> anyhow::Result<()> {
-    playerctl("play");
-    Ok(())
-}
-
-#[cfg(not(any(target_os = "windows", target_os = "linux")))]
-pub fn play() -> anyhow::Result<()> {
-    Ok(())
-}
-
-#[cfg(target_os = "windows")]
 pub fn pause() -> anyhow::Result<()> {
     use windows::Win32::UI::Input::KeyboardAndMouse::VK_MEDIA_PLAY_PAUSE;
     send_key(VK_MEDIA_PLAY_PAUSE);
     Ok(())
 }
 
-#[cfg(target_os = "linux")]
-pub fn pause() -> anyhow::Result<()> {
-    playerctl("pause");
-    Ok(())
-}
-
-#[cfg(not(any(target_os = "windows", target_os = "linux")))]
-pub fn pause() -> anyhow::Result<()> {
-    Ok(())
-}
-
-#[cfg(target_os = "windows")]
 pub fn next() -> anyhow::Result<()> {
     use windows::Win32::UI::Input::KeyboardAndMouse::VK_MEDIA_NEXT_TRACK;
     send_key(VK_MEDIA_NEXT_TRACK);
     Ok(())
 }
 
-#[cfg(target_os = "linux")]
-pub fn next() -> anyhow::Result<()> {
-    playerctl("next");
-    Ok(())
-}
-
-#[cfg(not(any(target_os = "windows", target_os = "linux")))]
-pub fn next() -> anyhow::Result<()> {
-    Ok(())
-}
-
-#[cfg(target_os = "windows")]
 pub fn prev() -> anyhow::Result<()> {
     use windows::Win32::UI::Input::KeyboardAndMouse::VK_MEDIA_PREV_TRACK;
     send_key(VK_MEDIA_PREV_TRACK);
-    Ok(())
-}
-
-#[cfg(target_os = "linux")]
-pub fn prev() -> anyhow::Result<()> {
-    playerctl("previous");
-    Ok(())
-}
-
-#[cfg(not(any(target_os = "windows", target_os = "linux")))]
-pub fn prev() -> anyhow::Result<()> {
     Ok(())
 }
