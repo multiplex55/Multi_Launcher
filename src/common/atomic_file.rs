a02c6572bc6217edacb87d48b0e31cfe704842d0
multiplex55 <6619098+multiplex55@users.noreply.github.com>
2026-07-19 16:51:00 -0400

    Use Windows API result for atomic replace

--- src/common/atomic_file.rs
+++ src/common/atomic_file.rs
@@ -95,22 +95,18 @@ fn replace_existing(src: &Path, dst: &Path) -> Result<()> {
     }
     let s = wide(src);
     let d = wide(dst);
-    let replaced = unsafe {
+    unsafe {
         MoveFileExW(
             PCWSTR(s.as_ptr()),
             PCWSTR(d.as_ptr()),
             MOVEFILE_REPLACE_EXISTING | MOVEFILE_WRITE_THROUGH,
         )
-    };
-    if replaced.as_bool() {
-        Ok(())
-    } else {
-        Err(std::io::Error::last_os_error()).with_context(|| {
-            format!(
-                "replace {} with {} using MoveFileExW",
-                dst.display(),
-                src.display()
-            )
-        })
     }
+    .with_context(|| {
+        format!(
+            "replace {} with {} using MoveFileExW",
+            dst.display(),
+            src.display()
+        )
+    })
 }
